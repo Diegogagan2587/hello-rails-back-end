c3448e76e7d348c20aea136ebd17f7a0119a3295
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-11 08:57:54 -0600

    routes.rb: root 'greetings#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'greetings#index'
+
   get 'greetings/index'
   get "up" => "rails/health#show", as: :rails_health_check
   get "/greetings" => "greetings#index" 
