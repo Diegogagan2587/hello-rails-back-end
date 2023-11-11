a914f0df06922c78c324d7d64bd713e67673c06a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-11 09:09:01 -0600

    Render JSON with random greeting

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,7 @@
 class GreetingsController < ApplicationController
   def index
+    # We get a randon greeting from the database
+    @greetings = Greeting.order("RANDOM()").first
+    render json: @greetings
   end
 end
